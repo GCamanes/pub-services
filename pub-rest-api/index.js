924fd2b2120c06d8c955d8a8fb353357d3e097a0
GuillaumeC <guillaume.camanes@gmail.com>
2018-12-12 10:01:10 +0100

    ajout de pub-rest-api: serveur pour requéter l'api depuis un browser

---
+++ pub-rest-api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const pubService = require('pub-services').services.pubService;
+const app = express();
+
+app.get('/pubs', function (req, res) {
+	const pubs = pubService.getAllPubs()
+	res.status(200).json(pubs);
+})
+
+app.get('/openpubs', function (req, res) {
+	const pubs = pubService.getAllOpenPubsToday()
+	res.status(200).json(pubs);
+})
+
+var server = app.listen(3000, function() {
+	console.log('Listening on port %d', server.address().port);
+});
